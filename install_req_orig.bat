da04abce2ce93b9612058ecf0185061e4835bcc7
angelrh97 <angelrivashidalgo@gmail.com>
2023-10-10 21:03:46 +0200

    Instalador de librerias y entorno virtual

---
+++ install_req_orig.bat
@@ -0,0 +1,10 @@
+python -m pip install --trusted-host pypi.org -U pip
+python -m pip install --trusted-host pypi.org -U virtualenv
+python -m virtualenv "%~dp0.venv_master"
+call "%~dp0.venv_master\Scripts\activate.bat"
+python -m pip install --trusted-host pypi.org -r req_orig.txt
+@echo off
+echo.
+echo.
+echo Installation finished
+pause
