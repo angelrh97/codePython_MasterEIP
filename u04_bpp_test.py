88d869cf1cc2ce726205ec07f93304d8e0381197
angelrh97 <angelrivashidalgo@gmail.com>
2023-10-13 16:14:49 +0200

    Creacion de las pruebas de areas y volumenes con "self"

--- u04_bpp_test.py
+++ u04_bpp_test.py
@@ -6,10 +6,17 @@ class TestMathOperations(unittest.TestCase):
     """
     Calculamos una operacion de volumen y de area para cada figura siendo radio o lado 2
     """
-    self.assertEqual(operacionesGeometricas('circulo', 5), math.pi*(5**2))
-    self.assertEqual(operacionesGeometricas('triangulo', 2, 3), 2*3*0.5)
-    self.assertEqual(operacionesGeometricas('cuadrado', 2), 2**2)
-    self.assertEqual(operacionesGeometricas('cuadrado', 3), 3**3)
+    def area_circulo(self):
+        self.assertEqual(operacionesGeometricas('circulo', 5), math.pi*(5**2))
+
+    def area_triangulo(self):
+        self.assertEqual(operacionesGeometricas('triangulo', 2, 3), 2*3*0.5)
+
+    def area_cuadrado(self):
+        self.assertEqual(operacionesGeometricas('cuadrado', 2), 2**2)
+
+    def volumen_cubo(self):
+        self.assertEqual(operacionesGeometricas('cuadrado', 3), 3**3)
 
 if __name__ == '__main__':
     unittest.main()
