0fa90669233cf2d60b03365dff5f60794d08d8e6
angelrh97 <angelrivashidalgo@gmail.com>
2023-10-13 17:54:37 +0200

    Reorganizacion de carpetas

--- u04/archivos_py_u04/install_req.bat
+++ u04/archivos_py_u04/install_req.bat
@@ -1,7 +1,7 @@
 python -m pip install --trusted-host pypi.org -U pip
 python -m pip install --trusted-host pypi.org -U virtualenv
-python -m virtualenv "%~dp0.venv_master"
-call "%~dp0.venv_master\Scripts\activate.bat"
+python -m virtualenv "%~dp0\..\venv_master"
+call "%~dp0\..\.venv_master\Scripts\activate.bat"
 python -m pip install --trusted-host pypi.org -r req_orig.txt
 @echo off
 echo.
